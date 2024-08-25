d33709aaa1531178cb580a507fa71c7fa52b51f0
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-25 17:25:23 +0200

    Updated code.

--- View_Users/View_users.py
+++ View_Users/View_users.py
@@ -6,17 +6,32 @@ import Main_Code.main
 def list_users_volume():
     Main_Code.main.clean_console()
 
-    # This is the PATH inside the Docker Container Volume
-    path_volume_docker = "/Docker_Directory/Storage/User_Data.txt"
+    # This is the PATH inside the Project Directory (current directory)
+    # -> Python_App_Using_Kubernetes/Store_Data/Store_data.py
+    absolutepath = os.path.abspath(__file__)
 
-    # Check if the directory inside the volume exist or not.
-    directory = os.path.dirname(path_volume_docker)
-    if not os.path.exists(directory):
-        print(f"The directory {directory} was not found")
+    # Go up one level -> Python_App_Using_Kubernetes/Store_Data
+    one_level_up = os.path.dirname(absolutepath)
+
+    # Go up two levels -> Python_App_Using_Kubernetes
+    two_level_up = os.path.dirname(one_level_up)
+
+    # Check if the directory inside the project exist or not.
+    # In case it doesn't exist, it is created.
+    directory_storage = os.path.join(two_level_up, "Storage")
+
+    # Name of the file will contain the user's data.
+    file_name = "Data_Users.txt"
+
+    # Path of the txt file where the user's data will stored
+    file_path = os.path.join(directory_storage, file_name)
+
+    if not os.path.exists(directory_storage):
+        print(f"The directory {directory_storage} was not found")
 
     # Try statesman to read all the file "USER_DATA" into the Docker volume
     try:
-        with open(path_volume_docker, 'r') as storage_file:
+        with open(file_path, 'r') as storage_file:
             content = storage_file.read()
             print("List Users:", end="\n")
             print(content)
