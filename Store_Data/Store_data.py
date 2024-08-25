08de1b10168590e6fc443a9bbd606839e78a28e3
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-25 11:35:15 +0200

    Updated code.

--- Store_Data/Store_data.py
+++ Store_Data/Store_data.py
@@ -4,24 +4,37 @@ import os
 
 
 def Store_Data_Into_Volume(user):
-    # This is the PATH inside the Docker Container Volume
-    path_volume_docker = "/Docker_Directory/Storage/User_Data.txt"
+    # This is the PATH inside the Project Directory (current directory)
+    # -> Python_App_Using_Kubernetes/Store_Data/Store_data.py
+    absolutepath = os.path.abspath(__file__)
 
-    # Check if the directory inside the volume exist or not.
+    # Go up one level -> Python_App_Using_Kubernetes/Store_Data
+    one_level_up = os.path.dirname(absolutepath)
+
+    # Go up two levels -> Python_App_Using_Kubernetes
+    two_level_up = os.path.dirname(one_level_up)
+
+    # Check if the directory inside the project exist or not.
     # In case it doesn't exist, it is created.
-    directory = os.path.dirname(path_volume_docker)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        print(f"Created directory: {directory}")
+    directory_storage = os.path.join(two_level_up, "Storage")
+
+    # Name of the file will contain the user's data.
+    file_name = "Data_Users.txt"
+
+    file_path = os.path.join(directory_storage, file_name)
+
+    if not os.path.exists(directory_storage):
+        os.makedirs(directory_storage)
+        print(f"Created directory: {directory_storage}")
 
     try:
-        with open(path_volume_docker, 'a+') as storage_file:
+        with open(file_path, 'a+') as storage_file:
             storage_file.write(f"Name: {user.get_name()}\n")
             storage_file.write(f"Surname: {user.get_surname()}\n")
             storage_file.write(f"Address: {user.get_address()}\n")
             storage_file.write(f"Phone Number: {user.get_phone_number()}\n\n")
     except FileNotFoundError:
-        with open(path_volume_docker, 'w+') as storage_file:
+        with open(file_path, 'w+') as storage_file:
             storage_file.write(f"Name: {user.get_name()}\n")
             storage_file.write(f"Surname: {user.get_surname()}\n")
             storage_file.write(f"Address: {user.get_address()}\n")
